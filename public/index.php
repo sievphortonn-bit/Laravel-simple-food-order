767cbd0331c74b9481701701d4b20215bbdc2fbe
Sophaul <sophaul9999@gmail.com>
2025-11-30 08:47:38 +0700

    udate index final

--- public/index.php
+++ public/index.php
@@ -16,6 +16,9 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
 /*
 |--------------------------------------------------------------------------
